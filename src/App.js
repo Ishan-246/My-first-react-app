e35683e1aca3e971be7dd8292688bf0d73f83005
unknown <ishan.dadape@gmail.com>
2025-06-19 16:23:08 +0530

    Added Hide/Show Text Generator

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React, { useState } from 'react';
 
+function App() {
+  const [text, setText] = useState('');
+  const [visible, setVisible] = useState(true);
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <input 
+        value={text} 
+        onChange={(e) => setText(e.target.value)} 
+      />
+      <br />
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? 'Hide' : 'Show'}
+      </button>
+      <br />
+      {visible && <p>{text}</p>}
+    </div>
+  );
+}
+export default App;
